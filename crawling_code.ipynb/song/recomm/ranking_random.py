e3a3e9fadbebfdd2bde614058824266ed639641f
yeji0701 <dpwl0701@gmail.com>
2020-12-10 19:31:38 +0900

    update code and add explanations

--- crawling_code.ipynb/song/recomm/ranking_random.py
+++ crawling_code.ipynb/song/recomm/ranking_random.py
@@ -7,13 +7,12 @@ import random
 import song.user.user_playlist as playlist
 
 # mongodb에 연결해서 dataframe 만들기 : 전체곡 정보 데이터프레임 : df
-client = pymongo.MongoClient('mongodb://dss:dss@'ip':27017')
+client = pymongo.MongoClient('mongodb://dss:dss@3.35.112.78:27017')
 db = client['melon_chart']
 collection = db.song_list
 df = pd.DataFrame(list(collection.find()))
 df.drop(columns=['_id'], inplace=True)
 
-# 1. 전체인기곡 & 유저플레이리스트 장르유사도 top5 -> random플레이리스트 만들기
 
 # 장르선호도가 유사한 연도 ranking top5 뽑는 함수
 def top5_list(seq, df=df):
@@ -21,29 +20,36 @@ def top5_list(seq, df=df):
     recommend = []
     recommend_df = pd.DataFrame()
     
-    # 2. 전체인기곡들 장르유사도
+    # 1. 전체인기곡들 장르유사도
+    df = df[df["Year"] >= 1980].reset_index(drop = True)
+    # MAKE YEAR-RANGE
+    ## elements 
+    bins = round((np.max(df["Year"]) - np.min(df["Year"]))/4)
+    labels = []
+    for i in (range(np.min(df["Year"]) , np.max(df["Year"]) +1,4)):
+        labels.append(str(i) + '-' + str(i+3))
+    # DIVIDED BY 4YR-CYCLE
+    ## ex) 1980-1983 , ... ,2016-2019
+    df['year_range'] = pd.cut(x=df['Year'], bins= bins ,labels=labels)
     df["count"] = 1
     df_pivot = pd.pivot_table(data = df , values = "count", index = "Year", columns = "Genre",aggfunc = np.sum)
     df_pivot.fillna(0, inplace = True)
     df_normal = round(df_pivot.div(df_pivot.sum(axis = 1), axis=0),3)
     df_normal = df_normal.reset_index()
     
-    # 3. 유저플레이리스트 장르유사도  함수
+    # 2. 유저플레이리스트 장르유사도  함수
     df_user = playlist.user(seq)
     df_user.reset_index()
-    # pivot!!
+    # PIVOT
     df_user["count"] = 1
     user_pivot = pd.pivot_table(data = df_user, values = "count", columns="Genre",  aggfunc = np.sum)
     user_pivot.fillna(0, inplace=True)
-    # 특정장르곡수/전체장르곡수 나눈 값 df : 사용자플레이리스트의 장르선호도 df
+    # 특정장르곡수/전체장르곡수 나눈 값 df : 사용자플레이리스트의 장르유사도 df
     user_normal = round(user_pivot.div(user_pivot.sum(axis=1), axis=0), 3)
     user_normal = pd.DataFrame(user_normal, columns=['R&B/Soul', '국내영화/국내드라마', '그외', '댄스','랩/힙합','록/메탈','발라드','성인가요','인디음악','재즈','포크/블루스'])
     np.nan_to_num(user_normal, copy=False)
 
-
-    # # NEW FRAME SUB YEAR
-    # df_normal = df_normal.drop('Year',1)
-
+    # 3. 장르유사도 비교 : 전체인기곡들 & 유저플레이리스트
     # 오차 구하기
     for i in range(len(df_normal)):
         error = (df_normal.loc[i] - user_normal) ** 2
@@ -51,13 +57,12 @@ def top5_list(seq, df=df):
         error = np.sqrt(error)
         recommend.append(error)
         
-    # rms 칼럼만 만들기 : 컬럼명 count를 rms로 변경
+    # rms 칼럼만 만들기, 컬럼명 count를 rms로 변경
+    from pandas import DataFrame
     recommend_df = pd.DataFrame(recommend)
     recommend_df.rename(columns={"count":"rms"}, inplace=True)
     
-    
-    # recommend_df 에 Year 값 넣은 df 만들기
-    # df_normal에서 Year 컬럼 선택
+    # recommend_df 에 Year 값 넣은 df 만들기, df_normal에서 Year 컬럼 선택
     year = pd.DataFrame(df_normal["Year"])
     
     # Year 컬럼있는 df와 rms 컬럼있는 df 합치기
@@ -65,7 +70,6 @@ def top5_list(seq, df=df):
     
     # 유사도 상위5개 : 인덱스 수정하기 위해 .reset_index(drop=True)
     ranking = recom.sort_values(by="rms", ascending=True).reset_index(drop=True)
-    
     
     # 상위 5개 뽑기
     top5_r = pd.DataFrame()
